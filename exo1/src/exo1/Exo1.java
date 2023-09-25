0e3bd0300514a9c45ab43b87c0d8b1dbcf396bdd
chloé sevry <chloe.sevry@gmail.com>
2023-09-25 11:15:05 +0200

    ajout exos calculator et exos 1 et 2

---
+++ exo1/src/exo1/Exo1.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package exo1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author sevry chloé TDC découverte de NetBeans 25/09/2023
+ */
+public class Exo1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Bonjour");
+        System.out.println("aurevoir");
+        String prenom;
+        Scanner sc ;
+        = new Scanner(System.in);
+        System.out.println("Bonjour, quel est votre prenom ?");
+        prenom = sc.nextLine();
+    }
+    
+}
