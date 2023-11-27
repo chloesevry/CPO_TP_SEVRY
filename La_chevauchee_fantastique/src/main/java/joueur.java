3983e7c21c6ae81ff360479cdf9fd6157906fa74
chloé sevry <chloe.sevry@gmail.com>
2023-11-27 10:21:55 +0100

    debut projet

---
+++ La_chevauchee_fantastique/src/main/java/joueur.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author bdom
+ */
+public class joueur {
+    
+    
+    
+    String Nom;
+    String Couleur;
+    int level = 1;
+    Cavalier cavalier;
+    
+    public joueur(String nom, String couleur){
+        Nom = nom;
+        Couleur = couleur;
+        cavalier = new Cavalier(Couleur);
+    }
+    
+    public void set_color(String couleur){
+        Couleur = couleur;
+    }
+}
+    
+
+
+    
+
