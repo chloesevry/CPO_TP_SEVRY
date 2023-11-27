ea7ad48a2f407df19fc9cdbc099eb66e238c817e
chloé sevry <chloe.sevry@gmail.com>
2023-11-27 10:24:01 +0100

    debut projet

---
+++ La_chevauchee_fantastique/src/main/java/Cavalier.java
@@ -0,0 +1,32 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author sevry
+ */
+public class Cavalier {
+    
+    
+    
+    String Couleur;
+    String Name = "Cavalier";
+    int X;
+    int Y;
+    
+    public Cavalier(String couleur){
+        Couleur = couleur;
+    }
+    
+    public void set_coords(int x, int y){
+        X = x;
+        Y = y;
+    }
+    
+}
+    
+
+    
+
